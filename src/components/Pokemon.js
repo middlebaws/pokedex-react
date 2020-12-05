1c0bb3007b3811e4e396594f01989d16f5f58338
middlebaws <otaviosk9@gmail.com>
2020-12-05 15:57:47 -0300

    Style Changes

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,27 +1,48 @@
 import React from 'react';
+import './Pokemon.css';
+import { Card, CardImg, CardBody, Button } from 'reactstrap';
 
 export function Pokemon({ pokemon }) {
     return (
-        <div className="pokemon-card">
+        <div className="col-md-4 col-xs-12 chard" >
+        <Card>
             <div className="pokemon-card-name">
-                <p>{pokemon.name}</p>
+            <p>{pokemon.name}</p>
             </div>
-            <div className="pokemon-card-type">
-                <span>{pokemon.types[0]}</span>
-                <span>{pokemon.types[1]}</span>
-            </div>
-            <div className="pokemon-card-stats">
+            <div className="pokemon-card-meta">
                 <span>{pokemon.maxHP}</span>
                 <span>{pokemon.maxCP}</span>
             </div>
             <div className="pokemon-card-image">
-                <img src={pokemon.image} alt={pokemon.name} />
+            <CardImg className="pokemon-card-image" top src={pokemon.image} alt={pokemon.name} />
             </div>
+            <CardBody>
             <div className="pokemon-card-attacks">
                 {pokemon.attacks.special.slice(0, 3).map(attack => (
-                    <span key={`${attack.name}-${attack.damage}`}>{attack.name}</span>
+                <Button key={`${attack.name}-${attack.damage}`}>{attack.name}</Button>
                 ))}
             </div>
-        </div>
+            </CardBody>
+        </Card>
+      </div>
     );
-};
+}
+
+
+/* <div className="pokemon">
+<div className="pokemon-card-name">
+    <p>{pokemon.name}</p>
+</div>
+<div className="pokemon-card-meta">
+    <span>{pokemon.maxHP}</span>
+    <span>{pokemon.maxCP}</span>
+</div>
+<div className="pokemon-card-image">
+    <img src={pokemon.image} alt={pokemon.name} />
+</div>
+<div className="pokemon-card-attacks">
+    {pokemon.attacks.special.slice(0, 3).map(attack => (
+        <span key={`${attack.name}-${attack.damage}`}>{attack.name}</span>
+    ))}
+</div>
+</div> */
--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -5,7 +5,7 @@ import { GET_POKEMONS } from '../graphql/get-pokemons';
 
 export function PokemonsContainer() {
     const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
-        variables: { first: 9 },
+        variables: { first: 151 },
     });
     
     return (
